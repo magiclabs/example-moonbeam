1f5850ef560b392a15e45a6109c43a871efe5458
Richard Oh <richard@magic.link>
2022-09-14 08:58:23 -0500

    formats style rules in readme and magic

--- src/magic.js
+++ src/magic.js
@@ -3,17 +3,17 @@ import Web3 from 'web3';
 
 const customNodeOptions = {
   rpcUrl: 'https://rpc.testnet.moonbeam.network',
-  chainId: 1287
-}
+  chainId: 1287,
+};
 
 // Setting network to Moonbeam Testnet
 export const magicMoonbeam = new Magic(
   process.env.REACT_APP_MAGIC_PUBLISHABLE_KEY, 
   { 
-    network: customNodeOptions 
-  }
+    network: customNodeOptions,
+  },
 );
-magicMoonbeam.network = 'moonbeam'
+magicMoonbeam.network = 'moonbeam';
 
 export const web3Moonbeam = new Web3(magicMoonbeam.rpcProvider);
 
@@ -25,8 +25,8 @@ export const magicEthereum = new Magic(
       rpcURL: 'https://eth-goerli.g.alchemy.com/v2/K2PvBhxr80z5cRNJ9-Kc-PG2GiLJaU6l',
       chainId: 5,
     },
-  }
+  },
 );
-magicEthereum.network = 'ethereum'
+magicEthereum.network = 'ethereum';
 
 export const web3Ethereum = new Web3(magicEthereum.rpcProvider);
